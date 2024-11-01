667057b0f8e7d4eb17b7dfdee38eff30beb0d3f9
Dev-Benicio <161643883+Dev-Benicio@users.noreply.github.com>
2024-11-01 16:25:19 -0300

    Update Database.php

--- app/database/Database.php
+++ app/database/Database.php
@@ -1,3 +1,12 @@
-<?php
-
 # Classe para conexão e manipulação do banco de dados
+<?php
+        $host= "127.0.0.1";
+        $user = "root";
+        $pass = "";
+        $db_name = "loja_moto";
+        $link = mysqli_connect($host, $user, $pass, $db_name);
+        
+        if (!$link) {
+            die("Error!! ". mysqli_error());
+        }
+?>
