f73e1cc9de990a9a6b58cc6d5671abd95ab491ce
Dev-Benicio <161643883+Dev-Benicio@users.noreply.github.com>
2024-11-01 16:21:19 -0300

    codigo para a Connection.php

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -1,1 +1,11 @@
-
+<?php
+        $host= "127.0.0.1";
+        $user = "root";
+        $pass = "";
+        $db_name = "loja_moto";
+        $link = mysqli_connect($host, $user, $pass, $db_name);
+        
+        if (!$link) {
+            die("Error!! ". mysqli_error());
+        }
+?>
