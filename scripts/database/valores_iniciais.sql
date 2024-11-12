025b00e8844a4d4e730c7a234924ff929bec1915
Niel-De <168747250+Niel-De@users.noreply.github.com>
2024-11-12 00:01:28 -0300

    Update valores_iniciais.sql
    Adicionei mais 2 valores

--- scripts/database/valores_iniciais.sql
+++ scripts/database/valores_iniciais.sql
@@ -23,3 +23,63 @@ INSERT INTO `moto`
 VALUES(0, "Yamaha", "TX 8090", "combustão", 13090.560, "10", "10km/L");
 
 INSERT INTO `venda` VALUES(0, "PIX", 12890.000, "2024-11-07", 0, 0);
+
+-- -----------------------------------------------------
+-- Segundo - cliente
+-- -----------------------------------------------------
+
+INSERT INTO `cliente` VALUES (0, "Aloísio Chulapa", "11111111111", "Naquela outra rua", "61999999999", "Alu.Chulapaa@gmail.com", '1984-04-20');
+
+SELECT * from cliente;
+
+INSERT INTO `funcionario`
+	(ID_funcionario, login, senha, nome, CPF, endereco, telefone, email, cargo, data_admissao, salario)
+VALUES (
+  '0',
+  'admin',
+  'root',
+  'Marcos Demacol', 
+  '33333333333',
+  'São Paulo',
+  '61988888888',
+  'sentiFirmeza@gmail.com',
+  'administrador',
+  '2024-11-07',
+  1900.500
+);
+
+INSERT INTO `moto`
+	(ID_moto, marca, modelo, tipo_motor, preco, potencia_cavalos, consumo_km)
+VALUES(0, "Honda", "CG 160 Titan", "combustão", 13090.560, "10", "10km/L");
+
+INSERT INTO `venda` VALUES(0, "Credito", 12890.000, "2024-11-07", 0, 0);
+
+-- -----------------------------------------------------
+-- Terceiro - cliente
+-- -----------------------------------------------------
+
+INSERT INTO `cliente` VALUES (0, "Junior filho de junior", "11111111111", "Ali naquela rua", "61999999999", "Junior.FJunior@gmail.com", '2000-04-12');
+
+SELECT * from cliente;
+
+INSERT INTO `funcionario`
+	(ID_funcionario, login, senha, nome, CPF, endereco, telefone, email, cargo, data_admissao, salario)
+VALUES (
+  '0',
+  'admin',
+  'root',
+  'Leandro Marmota', 
+  '77777777777',
+  'Rio de Janeiro',
+  '61988888888',
+  'marmotaaa.Lea@gmail.com',
+  'administrador',
+  '2024-11-07',
+  1900.500
+);
+
+INSERT INTO `moto`
+	(ID_moto, marca, modelo, tipo_motor, preco, potencia_cavalos, consumo_km)
+VALUES(0, "Kawazaki", "Ninja  ZX-10R", "combustão", 13090.560, "10", "10km/L");
+
+INSERT INTO `venda` VALUES(0, "Credito", 120190.000, "2024-11-07", 0, 0);
