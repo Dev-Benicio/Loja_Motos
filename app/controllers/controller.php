880e9e0694a58dbd00c3c5e76783dec2e9f2ea77
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-11-18 12:42:59 -0300

    método call_view agora recebe parâmetros que podem ser utilizados pela view

--- app/controllers/controller.php
+++ app/controllers/controller.php
@@ -6,9 +6,23 @@ abstract class controller
 {
   abstract public function index();
 
-  public function call_view(string $view_name)
+  /**
+   * Chama uma view e passa, quando tiver, os parâmetros para serem acessados na view.
+   * @param string $view_name Nome da view que deseja chamar.
+   * @param array $params Array associativo com os parâmetros a serem passados.
+   */
+  public function call_view(string $view_name, array $params = []): void
   {
     $caminho_view = "app/views/{$view_name}.php";
-    file_exists($caminho_view) && require_once $caminho_view;
+
+    if (!file_exists($caminho_view)) {
+      require_once "app/views/error_404.php";
+      exit;
+    }
+
+    // Cada elemento do array associativo é transformado em uma variável
+    extract($params);
+    require_once $caminho_view;
+    exit;
   }
 }
