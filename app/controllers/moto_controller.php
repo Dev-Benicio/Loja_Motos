c6a1b788068d9d16018a4df88ca10f4552379048
Niel-De <168747250+Niel-De@users.noreply.github.com>
2024-11-19 17:01:43 -0300

    Update moto_controller.php

--- app/controllers/moto_controller.php
+++ app/controllers/moto_controller.php
@@ -21,7 +21,17 @@ class moto extends controller
     $this->call_view('edicao_motos');
   }
 
-  public function cadastrar() {}
+  public function cadastrar() {
+    $marca = $_POST["marca"];
+  $modelo = $_POST["modelo"];
+  $combustivel = $_POST["combustivel"];
+  $preco = $_POST["preco"];
+  $ano = $_POST["ano"];
+  $potencia_cavalos = $_POST["potencia_cavalos"];
+  $consumo_km = $_POST ["consumo_km"];
+  $estoque = $_POST ["estoque"];
+  $foto_moto = $_POST ["foto_moto"];
+  }
 
   public function listar(int $id = null) {}
 
