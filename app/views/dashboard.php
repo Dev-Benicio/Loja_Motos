72638c0895580be7e0351280c390f1c0a0b9fe65
Ítalo <167659256+mandioca-frita@users.noreply.github.com>
2024-11-27 07:49:57 -0300

    clean: limpado o código html e php da view dashboard

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -16,12 +16,11 @@
 
   <!-- Bento Grid com cards de relatórios -->
   <main class="container mt-4">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <div class="row mb-3">
-        <div
-          class="col position-relative"
-          style="min-height: 300px; max-height: 350px;"
-        >
+      <div
+        class="col position-relative"
+        style="min-height: 300px; max-height: 350px;"
+      >
         <img
           src="./assets/images/motocross_dashboard.jpg"
           alt="Pessoa em uma motocicleta de motocross fazendo uma manobra no alto, depois de uma rampa"
@@ -29,7 +28,8 @@
         <!-- Top 3 funcionários -->
         <ul
           class="d-flex flex-column justify-content-center gap-3 list-group position-absolute top-0 end-0 h-100"
-          style="margin-right: 2em; scale: 0.95;">
+          style="margin-right: 2em; scale: 0.95;"
+        >
           <li class="list-group-item active bg-black border-0 text-white pt-1 pb-2 px-4 rounded-3">
             <span class="fw-medium fs-4">
               Os mais Thunders!
@@ -38,108 +38,54 @@
               Esses são os vendedores destaque do mês.
             </p>
           </li>
-          <!-- Lista dos top 3 funcionários -->
-          <li class="d-flex align-items-center gap-3 list-group-item border-0 rounded-3" style="padding: 0.8rem 0.75rem;">
-            <!-- Avatar -->
-            <div class="position-relative">
-              <div
-                class="d-flex align-items-center justify-content-center bg-body-secondary rounded-circle p-2"
-                style="height: 50px; width: 50px;">
-                <i class="bi bi-person fs-2 text-secondary"></i>
-              </div>
-              <div class="position-absolute top-50 start-50 ms-1 d-flex align-items-center justify-content-center">
-                <i class="bi bi-award fs-3"></i>
-              </div>
-            </div>
-            <!-- Informações do funcionário -->
-            <div class="d-flex justify-content-between w-100">
-              <div class="d-flex flex-column justify-content-center">
-                <span class="fw-semibold" style="font-size: 1.15rem; line-height: 1.15rem;">
-                  Júlia Silva
-                </span>
-                <p class="mb-0 text-secondary">
-                  1° Lugar
-                </p>
-              </div>
-              <div class="d-flex flex-column justify-content-center align-items-end  me-2">
-                <span class="fw-semibold fs-6">
-                  Vendas
-                </span>
-                <p class="mb-0" style="font-size: 1.05rem;">
-                  R$ 1900,65
-                </p>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex align-items-center gap-3 list-group-item border-0 rounded-3" style="padding: 0.8rem 0.75rem;">
-            <!-- Avatar -->
-            <div class="position-relative">
-              <div
-                class="d-flex align-items-center justify-content-center bg-body-secondary rounded-circle p-2"
-                style="height: 50px; width: 50px;">
-                <i class="bi bi-person fs-2 text-secondary"></i>
-              </div>
-              <div class="position-absolute top-50 start-50 ms-1 d-flex align-items-center justify-content-center">
-                <i class="bi bi-award fs-3"></i>
-              </div>
-            </div>
-            <!-- Informações do funcionário -->
-            <div class="d-flex justify-content-between w-100">
-              <div class="d-flex flex-column justify-content-center">
-                <span class="fw-semibold" style="font-size: 1.15rem; line-height: 1.15rem;">
-                  Júlia Silva
-                </span>
-                <p class="mb-0 text-secondary">
-                  1° Lugar
-                </p>
-              </div>
-              <div class="d-flex flex-column justify-content-center align-items-end  me-2">
-                <span class="fw-semibold fs-6">
-                  Vendas
-                </span>
-                <p class="mb-0" style="font-size: 1.05rem;">
-                  R$ 1900,65
-                </p>
-              </div>
-            </div>
-          </li>
-          <li class="d-flex align-items-center gap-3 list-group-item border-0 rounded-3" style="padding: 0.8rem 0.75rem;">
-            <!-- Avatar -->
-            <div class="position-relative">
-              <div
-                class="d-flex align-items-center justify-content-center bg-body-secondary rounded-circle p-2"
-                style="height: 50px; width: 50px;">
-                <i class="bi bi-person fs-2 text-secondary"></i>
-              </div>
-              <div class="position-absolute top-50 start-50 ms-1 d-flex align-items-center justify-content-center">
-                <i class="bi bi-award fs-3"></i>
-              </div>
-            </div>
-            <!-- Informações do funcionário -->
-            <div class="d-flex justify-content-between w-100">
-              <div class="d-flex flex-column justify-content-center">
-                <span class="fw-semibold" style="font-size: 1.15rem; line-height: 1.15rem;">
-                  Júlia Silva
-                </span>
-                <p class="mb-0 text-secondary">
-                  1° Lugar
-                </p>
-              </div>
-              <div class="d-flex flex-column justify-content-center align-items-end  me-2">
-                <span class="fw-semibold fs-6">
-                  Vendas
-                </span>
-                <p class="mb-0" style="font-size: 1.05rem;">
-                  R$ 1900,65
-                </p>
-              </div>
-            </div>
-          </li>
+          <?php
+            for ($i = 1; $i <= 3; $i++) {
+              echo <<<HTML
+              <li
+                class="d-flex align-items-center gap-3 list-group-item border-0 rounded-3"
+                style="padding: 0.8rem 0.75rem;"
+              >
+                <!-- Avatar -->
+                <div class="position-relative">
+                  <div
+                    class="d-flex align-items-center justify-content-center bg-body-secondary rounded-circle p-2"
+                    style="height: 50px; width: 50px;"
+                  >
+                    <i class="bi bi-person fs-2 text-secondary"></i>
+                  </div>
+                  <div class="position-absolute top-50 start-50 ms-1 d-flex align-items-center justify-content-center">
+                    <i class="bi bi-award fs-3"></i>
+                  </div>
+                </div>
+                <!-- Informações do funcionário -->
+                <div class="d-flex justify-content-between w-100">
+                  <div class="d-flex flex-column justify-content-center">
+                    <span class="fw-semibold" style="font-size: 1.15rem; line-height: 1.15rem;">
+                      Júlia Silva
+                    </span>
+                    <p class="mb-0 text-secondary">
+                      {$i}° Lugar
+                    </p>
+                  </div>
+                  <div class="d-flex flex-column justify-content-center align-items-end me-2">
+                    <span class="fw-semibold fs-6">
+                      Vendas
+                    </span>
+                    <p class="mb-0" style="font-size: 1.05rem;">
+                      R$ 1900,65
+                    </p>
+                  </div>
+                </div>
+              </li>
+              HTML;
+            }
+          ?>
         </ul>
       </div>
     </div>
 
     <div class="row d-flex gap-3">
+      <!-- Relatório de ocupação do estoque-->
       <div class="col-3 card rounded-4 shadow-sm p-3 position-relative">
         <div class="card-title d-flex align-items-center gap-3">
           <img
@@ -197,7 +143,6 @@
               75%
             </span>
           </div>
-          
         </div>
         <p class="text-center position-absolute translate-middle-x top-50 start-50 mt-4 p-3 w-100">
           O estoque está com 75% de ocupação
@@ -215,21 +160,18 @@
         </legend>
       </div>
 
-      <div class="card col-4 rounded-4">
+      <!-- Relatório de motores com maior rotação -->
+      <div class="card col-4 rounded-4 shadow-sm p-3">
         <div class="card-title">
           <span>Motos com maior rotatividade</span>
         </div>
       </div>
 
-      <div class="card col rounded-4">
+      <!-- Relatório de status de reposição -->
+      <div class="card col rounded-4 shadow-sm p-3">
         <span>Status de reposição</span>
       </div>
 
-      <div class="card col rounded-4">
-        <nav>
-          <!-- Outros relatórios -->
-        </nav>
-      </div>
     </div>
   </main>
 </div>
