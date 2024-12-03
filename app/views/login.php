ea812b18a250081d968bc629bf8c46736ff6188f
JO�O VICTOR SILVA MARQUES <36813x@etb.net.br>
2024-12-03 16:50:03 -0300

    refiz a página de login

--- app/views/login.php
+++ app/views/login.php
@@ -1,1 +1,40 @@
-<h1>Login</h1>
+<!-- Título do menu -->
+  <div class="d-flex flex-column ps-5">
+          <span class="status-text fw-light mb-0 fs-6">Login - Área Restrita </span>
+          <h2 class="brand-name fw-semibold fs-5 mt-1">Thunder Gears</h2>  
+    <main class="d-flex h-auto w-100"> 
+<!-- Formulário de cadastro-->
+  <div class="container">
+    <h1 class="brand-name fw-semibold fs-1 mt-1">Área Restrita</h1>  
+    <p class="fs-5">Entre com sua conta para continuar
+    </p>
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+    <!-- Imagem de um motociclista em sua moto -->
+<div class="foto">
+      <img src="assets/images/moto_login.jpg" alt="Foto de uma pessoa em cima de uma moto" class="object-fit-cover h-auto w-100">
+</div>
+    </div>
+        </main>
+
+
+
+
+
+
+
