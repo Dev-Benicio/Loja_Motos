ed90706422c05ab4473a6bab97ad0bb824f9bbd1
JO�O VICTOR SILVA MARQUES <36813x@etb.net.br>
2024-12-03 20:14:33 -0300

    Alterações finais

--- app/views/login.php
+++ app/views/login.php
@@ -1,38 +1,43 @@
-<!-- Título do menu -->
-  <div class="d-flex flex-column ps-5 h-vh ">
-          <span class="status-text fw-light mb-0 fs-6">Login - Área Restrita </span>
-          <h2 class="brand-name fw-semibold fs-5 mt-1">Thunder Gears</h2>  
-    <main class="d-flex  h-100 w-100"> 
-<!-- Formulário de cadastro-->
-  <div class="container">
-    <h1 class="brand-name fw-semibold fs-1 mt-1">Área Restrita</h1>  
-    <p class="fs-5">Entre com sua conta para continuar
-    </p>
-    <form>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
-    <!-- Imagem de um motociclista em sua moto -->
-<div id="foto">
-      <img src="assets/images/moto_login.jpg"
-       alt="Foto de uma pessoa em cima de uma moto" 
-       class="object-fit-cover h-100 w-100  rounded-4">
-</div>
+<div class="d-flex flex-column justify-content-center vh-100">
+  <main class="d-flex h-100 w-100">
+    <!-- Formulário de cadastro-->
+    <div class="d-flex flex-column justify-content-center align-items-center w-50">
+      <div class="d-flex flex-column position-absolute top-0 start-0">
+        <div id="titulo"
+        <span class="status-text fw-light mb-0 fs-6">Login - Área Restrita </span>
+        <h2 class="brand-name fw-semibold fs-5 mt-1">Thunder Gears</h2>
+         </div>
+      </div>
+      <!-- Título do formulário -->
+      <div class=""
+      <h1 class="brand-name fw-semibold fs-1 mt-1">Área Restrita</h1>
+      <p class="fs-5">Entre com sua conta para continuar</p>
+      <!-- Formulário de Login -->
+      <form class="w-75 d-grid flex-column gap-5" >
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Login</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Senha</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+          </div>
+          <div class="d-grid gap-2 col-6 mx-auto">
+            <button class="btn btn-primary" type="button">Entrar</button>
+      </div>
+      </form>
     </div>
-        </main>
+    <!-- Imagem de um motociclista em sua moto -->
+  <div class="h-100 w-50 image-container">
+    <div id="foto">
+      <img src="assets/images/moto_login.jpg"
+        alt="Foto de uma pessoa em cima de uma moto"
+        class="object-fit-contain border rounded h-100 w-100 rounded-5">
+        </div>
+  </div>
+    </div>
+  </main>
+</div>
 
 
 
