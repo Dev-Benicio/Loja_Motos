f1df273082464d1d8609f425a23e3c376db6efac
Niel-De <168747250+Niel-De@users.noreply.github.com>
2024-12-03 23:52:10 -0300

    Update database.sql

--- scripts/database/database.sql
+++ scripts/database/database.sql
@@ -97,19 +97,101 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
--- Views thunder_gears
+-- Views Modelo mais vendido
 -- -----------------------------------------------------
-CREATE VIEW `thunder_gears`.`vw_relatorio_mais_vendidos` AS
-SELECT
-	m.modelo,
-	COUNT(DISTINCT id_venda) AS total_venda
+CREATE VIEW `motos_mais_vendidos` AS
+SELECT 
+    m.modelo AS modelo,
+    COUNT(v.id_venda) AS venda
 FROM 
-	`venda`
-JOIN
-	`moto` m ON id_moto
-JOIN
-	 `venda` v ON id_venda 
-GROUP by
-	m.modelo
-ORDER by
-	total_vendido DESC;
+    vendas v
+LEFT JOIN 
+    moto m ON v.id_moto = m.id_moto
+GROUP BY 
+    m.modelo
+ORDER BY 
+    venda DESC;
+ 
+-- -----------------------------------------------------
+-- Views Modelo mais vendido
+-- ----------------------------------------------------- 
+CREATE VIEW vendas_por_vendedor AS
+SELECT 
+    f.id_funcionario AS vendedor_id,
+    f.nome AS vendedor_nome,
+    COUNT(v.id_venda) AS total_vendas,
+    SUM(v.valor_total_venda) AS total_valor_vendas
+FROM 
+    thunder_gears.vendas v
+JOIN 
+    thunder_gears.funcionario f ON v.id_funcionario = f.id_funcionario
+GROUP BY 
+    f.id_funcionario
+ORDER BY 
+    total_vendas DESC;
+
+-- -----------------------------------------------------
+-- Views Quantidade em estoque
+-- -----------------------------------------------------
+CREATE VIEW quantidade_em_estoque AS
+SELECT 
+    m.id_moto,
+    m.modelo,
+    m.marca,
+    m.quantidade_estoque,
+    m.status_moto
+FROM 
+    thunder_gears.moto m
+WHERE 
+    m.status_moto = 'disponivel' -- Filtrando apenas as motos disponíveis
+ORDER BY 
+    m.modelo;
+    
+-- -----------------------------------------------------
+-- Views Reposição do estoque
+-- -----------------------------------------------------    
+CREATE VIEW status_reposicao AS
+SELECT 
+    m.id_moto,
+    m.modelo,
+    m.marca,
+    m.quantidade_estoque,
+    m.status_moto,
+    CASE
+        WHEN m.quantidade_estoque < 5 THEN 'Precisa repor'
+        ELSE 'Estoque suficiente'
+    END AS status_reposicao,
+    '2024-12-10' AS previsao_chegada -- Essa é um data de exemplo
+FROM 
+    thunder_gears.moto m
+WHERE 
+    m.status_moto = 'disponivel'
+ORDER BY 
+    m.modelo;
+    
+-- -----------------------------------------------------
+-- Views Status funcionário
+-- -----------------------------------------------------    
+CREATE VIEW status_funcionario AS
+SELECT 
+    f.id_funcionario,
+    f.nome,
+    f.cargo,
+    f.status_funcionario,
+    f.data_demissao,
+    f.data_admissao,
+    f.salario,
+    f.telefone,
+    f.email,
+    f.foto_perfil,
+    CASE
+        WHEN f.status_funcionario = 'ativo' THEN 'Ativo'
+        WHEN f.status_funcionario = 'inativo' THEN 'Desligado'
+        WHEN f.status_funcionario = 'ferias' THEN 'Férias'
+        WHEN f.status_funcionario = 'afastado' THEN 'Afastado'
+        ELSE 'Status Indefinido' -- esse daqui é pra quando o cara foi demitido(teve férias permanentekkkkkkkkkkkk)
+    END AS descricao_status
+FROM 
+    thunder_gears.funcionario f
+ORDER BY 
+    f.nome;
