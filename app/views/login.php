bed73e302c8ba8a88796277b4288d9eab3572c2b
JO�O VICTOR SILVA MARQUES <36813x@etb.net.br>
2024-12-05 12:51:44 -0300

    pagina de login finalizada. Obs: Está sem a frases dentro da imagem

--- app/views/login.php
+++ app/views/login.php
@@ -3,38 +3,39 @@
     <!-- Formulário de cadastro-->
     <div class="d-flex flex-column justify-content-center align-items-center w-50">
       <div class="d-flex flex-column position-absolute top-0 start-0">
-        <div id="titulo"
+      <div id="titulo"
         <span class="status-text fw-light mb-0 fs-6">Login - Área Restrita </span>
         <h2 class="brand-name fw-semibold fs-5 mt-1">Thunder Gears</h2>
-         </div>
+      </div>
       </div>
       <!-- Título do formulário -->
-      <div class=""
-      <h1 class="brand-name fw-semibold fs-1 mt-1">Área Restrita</h1>
+      <h1 class="brand-name fw-semibold fs-1 mt-1 d-flex flex-column gap-3">Área Restrita</h1>
+      <div id="subtitle"
       <p class="fs-5">Entre com sua conta para continuar</p>
+      </div>
       <!-- Formulário de Login -->
-      <form class="w-75 d-grid flex-column gap-5" >
+      <form class="w-75 d-flex flex-column gap-5">
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Login</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Senha</label>
           <input type="password" class="form-control" id="exampleInputPassword1">
-          </div>
-          <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-primary" type="button">Entrar</button>
-      </div>
-      </form>
+        </div>
+        <div class="d-grid gap-2 col-6 mx-auto">
+          <button class="btn btn-primary" type="button">Entrar</button>
     </div>
+    </div>
+      </form>
+    
     <!-- Imagem de um motociclista em sua moto -->
-  <div class="h-100 w-50 image-container">
-    <div id="foto">
+    
+    <div class="h-100 w-50">
       <img src="assets/images/moto_login.jpg"
         alt="Foto de uma pessoa em cima de uma moto"
-        class="object-fit-contain border rounded h-100 w-100 rounded-5">
-        </div>
-  </div>
+        class="object-fit-cover border rounded h-100 w-100 rounded-5">
     </div>
   </main>
 </div>
